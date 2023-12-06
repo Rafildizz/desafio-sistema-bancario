e657985e725a540f4d4ca2b6ae1773b7c32ddeaa
Rafildizz <rafatel91@gmail.com>
2023-12-05 21:02:05 -0300

    Versão 1 - Desafio Sistema Bancário

---
+++ desafio_sistema_bancario.py
@@ -0,0 +1,49 @@
+menu = '''
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+'''
+
+saldo = 0
+limite = 500
+extrato = []
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+    
+    if opcao == 'd':
+        valor_deposito = float(input('Quando deseja depositar? '))
+        saldo += valor_deposito
+        extrato.append(f'Depósito: + {valor_deposito:.2f}')
+        print('Depositado com sucesso!')
+    
+    elif opcao == 's':
+        if numero_saques < LIMITE_SAQUES:
+            valor_saque = float(input('Quanto deseja sacar? '))
+            if 500 >= valor_saque <= saldo:
+                saldo -= valor_saque
+                extrato.append(f'Saque: - {valor_saque:.2f}')
+                print('Saque realizado com sucesso!')
+                numero_saques += 1
+            elif valor_saque > saldo:
+                print('Saldo insuficiente!')
+            else:
+                print('Valor maior do que o limite permitido!')
+        else:
+            print('Limite de saque diario excedido!')
+           
+    elif opcao == 'e':
+        print(f'\nExtrato: {saldo:.2f}')
+        for operacao in extrato:
+            print(operacao)
+    
+    elif opcao == 'q':
+        print('Operação finalizada!')
+        break
+
+    else:
+        print('Operação inválida, por favor selecione novamente a operação desejada.')
